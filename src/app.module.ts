abb198edb9e6c3ebb147386393eac0b9afc704b4
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-12-18 20:25:26 -0300

    Install prisma and create initial table

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma.server';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [PrismaService],
 })
 export class AppModule {}
