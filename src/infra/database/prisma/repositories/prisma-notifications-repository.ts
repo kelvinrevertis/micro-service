043928fd1845b18d2e562f00ae86725cc3499e45
Kelvin Reverts <kelvinrev2@hotmail.com>
2022-12-31 16:03:02 -0300

    Set modules

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -1,7 +1,9 @@
+import { Injectable } from '@nestjs/common/decorators';
 import { Notification } from 'src/application/entities/notification';
 import { NotificationsRepository } from 'src/application/repositories/notifications-repository';
 import { PrismaService } from '../prisma.service';
 
+@Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
   constructor(private prismaService: PrismaService) {}
 
